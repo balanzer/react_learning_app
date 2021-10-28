ddc1c4a8af7aa92abbe26c6d0c66b9c3e06df9bf
Murali Varath <muralitharan.balanzer@gmail.com>
2021-10-28 12:34:27 -0400

    added styles dynamic if input is invalid

--- react-styling-track-goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-styling-track-goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const goalInputChangeHandler = event => {
+        if (event.target.value.trim().length === 0) {
+            setIsValid(false);//To trigger error label
+        } else {
+            setIsValid(true);//To reset error label
+        }
+        setEnteredValue(event.target.value);
+    };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            return;
+        } else {
+            props.onAddGoal(enteredValue);
+        }
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+                <label>Course Goal</label>
+                <input
+                    type="text" onChange={goalInputChangeHandler}/>
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
